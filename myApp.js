4010979e0949dec7d67bb5157392b1be58f8208c
AkshatParmar <Akshat.parmar@rutgers.edu>
2021-09-30 22:08:58 -0400

    Challanege #10

--- myApp.js
+++ myApp.js
@@ -100,8 +100,11 @@ const removeById = (personId, done) => {
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-
-  done(null /*, data*/);
+  Person.remove({ name: nameToRemove }, (err, data) => {
+    if (err) return console.log(err);
+    console.log(data);
+    done(null, data);
+  });
 };
 
 const queryChain = (done) => {
